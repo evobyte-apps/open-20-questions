80b2b1534b4e8339157b261ca0abf9c9151413e9
Vlad-Sebastian Ionescu <ionescu.vlad1@gmail.com>
2021-12-30 22:44:00 +0000

    Fixed again allowed_hosts.

--- django/open_20q/settings.py
+++ django/open_20q/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # set in .env file
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['twentyq.evobyte.org/']
+ALLOWED_HOSTS = ['twentyq.evobyte.org']
 
 
 # Application definition
